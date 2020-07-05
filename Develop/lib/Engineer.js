66734cf2da0e46dc387f11afacd3e12a608ab8bc
Samir Bello <agnide4@gmail.com>
2020-07-05 15:29:40 -0500

    All classes created and exported

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+github  // GitHub username
+
+  * getGithub()
+
+  * getRole() // Overridden to return 'Engineer'
+
+class Engineer extends Employee {
+    constructor(github){
+        this.github = github
+    }
+    super(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getGithub(){
+        return this.github;
+    }
+    getRole(){
+        return Engineer;
+    }
+
+}
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+  const Employee = require("./Employee");
+
+  class Intern extends Employee{
+      constructor(school){
+          this.school = school;
+      }
+      super(name, id, email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+      }
+      getRole(){
+        return Intern;
+      }
+      getSchool(){
+          return this.school;
+      }
+  }
+
