fa3ccc31410fa2f0673e41ae4024676076b07cc8
Samir Bello <agnide4@gmail.com>
2020-07-06 12:27:38 -0500

    All questions created

--- Develop/app.js
+++ Develop/app.js
@@ -10,11 +10,11 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-let team_Action;
+//let team_Action;
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const teamCount = [
+const team_Action = [
     {
         type: "List",
         name: "Team options",
@@ -29,28 +29,91 @@ const q_Eng = [
     {
         type: "input",
         name: "Name",
-        message: "Please enter the employees name"
+        message: "Please enter the employee's name"
 
     },
     {
         type: 'Number',
-        id: "id",
+        name: "id",
         message: "What is the employee ID"
     },
     {
         type: 'input',
-        id: "email",
+        name: "email",
+        message: "What is the employee email address"
+    },
+    {
+        
+        type: 'input',
+        name: "github username",
+        message: "What is the engineer github username?"
+    }
+
+]
+
+const q_Mngr = [
+    {
+        type: "input",
+        name: "Name",
+        message: "Please enter the employee's name"
+
+    },
+    {
+        type: 'Number',
+        name: "id",
+        message: "What is the employee ID"
+    },
+    {
+        type: 'input',
+        name: "email",
         message: "What is the employee email address"
     },
     {
         
         type: 'Number',
-        id: "id",
-        message: "What is the employee ID"
+        name: "Office Number",
+        message: "What is the manager`s office number?"
     }
 
 ]
- 
+const q_Intern = [
+    {
+        type: "input",
+        name: "Name",
+        message: "Please enter the employee's name"
+
+    },
+    {
+        type: 'Number',
+        name: "id",
+        message: "What is the employee ID"
+    },
+    {
+        type: 'input',
+        name: "email",
+        message: "What is the employee email address"
+    },
+    {
+        
+        type: 'input',
+        name: "School",
+        message: "What is the intern`s school name?"
+    }
+
+]
+
+inquirer.prompt(team_Action).then(function(response1){
+    if (response1.choices[0]){
+        inquirer.prompt(q_Mngr)
+    }
+    else if (response1.choices[1]){
+        inquirer.prompt(q_Eng)
+    }else if (response1.choices[2]){
+        inquirer.prompt(q_Intern)
+    }else {
+        console.log("View team profile function here")
+    }
+})
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
